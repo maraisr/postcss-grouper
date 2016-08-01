b33886c98492cd976735d2f8dcb8c4678a2a6922
Marais Rossouw <me@maraisr.com>
2016-08-01 10:58:15 +1000

    chore: added uglifyjs and sourcemaps

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,2 @@
-"use strict";
-var postcss = require('postcss');
-module.exports = postcss.plugin('postcss-grouper', function (opts) {
-    if (opts === void 0) { opts = {}; }
-    return function (css) {
-    };
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQVksT0FBTyxXQUFNLFNBQVMsQ0FBQyxDQUFBO0FBRW5DLGlCQUFTLE9BQU8sQ0FBQyxNQUFNLENBQUMsaUJBQWlCLEVBQUUsVUFBQyxJQUFTO0lBQVQsb0JBQVMsR0FBVCxTQUFTO0lBQ3BELE1BQU0sQ0FBQyxVQUFDLEdBQUc7SUFFWCxDQUFDLENBQUM7QUFDSCxDQUFDLENBQUMsQ0FBQyJ9
+"use strict";var postcss=require("postcss");module.exports=postcss.plugin("postcss-grouper",function(s){return void 0===s&&(s={}),function(s){}});
+//# sourceMappingURL=dist/index.js.map
