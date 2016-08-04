17e90f9d998242e48a6853c30732c751e92dce6d
Marais Rossouw <me@maraisr.com>
2016-08-04 13:31:36 +1000

    test: changed it so that I didnt have to maintain test css files

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,45 +4,37 @@ import fs from 'fs';
 
 import plugin from '../dist';
 
-const run = (() => {
-	const filesCache = fs.readdirSync('./types/').map(v => {
-		return {
-			file: v,
-			io: [
-				fs.readFileSync(`./types/${v}/input.css`, 'utf8'),
-				fs.readFileSync(`./types/${v}/output.css`, 'utf8')
-			]
-		}
-	});
+const run = function (opts, a, b) {
+	let t = this;
 
-	return function (t, type, opts) {
-		let thisTest = void 0,
-			source = void 0,
-			dest = void 0;
+	return postcss([plugin(opts)])
+		.process(a)
+		.then(res => {
+			t.deepEqual(res.css, b);
+			t.deepEqual(res.warnings().length, 0);
+		});
+}
 
-		if (/[a-z][\/]+/.test(type)) {
-			thisTest = filesCache.filter(v => { return v.file == /([a-z])\//.exec(type)[1] })[0];
-			source = thisTest.io[0];
-			dest = thisTest.io[0];
-		} else {
-			thisTest = filesCache.filter(v => { return v.file == type })[0];
-			source = thisTest.io[0];
-			dest = thisTest.io[1];
-		}
-
-		return postcss([plugin(opts)])
-			.process(source)
-			.then(res => {
-				t.deepEqual(res.css, dest);
-				t.deepEqual(res.warnings().length, 0);
-			});
-	}
-})();
-
-test('groups with single regex', t => {
-	return run(t, 'a', { group: [/html/] });
+test('no options, then just pass through', t => {
+	const io = `
+html { color: black; }
+.foo { color: white; }
+html { margin-top: 10px; }
+	`;
+	return run.apply(t, [void 0, io, io]);
 });
 
-test('no options, no grouping', t => {
-	return run(t, 'a/input');
+test('groups with single regex', t => {
+	return run.apply(t, [{ group: [/html/] },
+		`
+html { color: black; }
+.foo { color: white; }
+html { margin-top: 10px; }
+	`,
+		`
+html { color: black; }
+html { margin-top: 10px; }
+.foo { color: white; }
+	`
+	])
 });
